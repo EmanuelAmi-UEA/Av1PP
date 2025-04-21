8f729dd2f242623d567b13f170669d42027be694
Pedro050723 <pedro_miranda_23@hotmail.com>
2025-04-21 23:13:20 +0000

    Assessment e Submission

---
+++ src/main/java/Assessment.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assessment {
+    private String tipo;
+    private double notaMaxima;
+    private double peso;
+    private Curso curso;
+    private List<Submission> submissions = new ArrayList<Submission>();
+
+    public Assessment(String tipo, double notaMaxima, double peso, Curso curso) {
+        this.tipo = tipo;
+        this.notaMaxima = notaMaxima;
+        this.peso = peso;
+        this.curso = curso;
+    }
+
+    public String getTipo() { 
+        return this.tipo; 
+    }
+    public double getNotaMaxima() { 
+        return this.notaMaxima; 
+    }
+    public double getPeso() { 
+        return this.peso; 
+    }
+    public Curso getCurso() { 
+        return this.curso; 
+    }
+    public List<Submission> getSubmissions() { 
+        return this.submissions; 
+    }
+
+    public void addSubmission(Submission submission) {
+        submissions.add(submission);
+    }
+}
---
+++ src/main/java/Submission.java
@@ -0,0 +1,31 @@
+public class Submission {
+    private Aluno aluno;
+    private Assessment assessment;
+    private double nota;
+    private String dataEntrega;
+    private String comentarios;
+
+    public Submission(Aluno aluno, Assessment assessment, double nota, String dataEntrega, String comentarios) {
+        this.aluno = aluno;
+        this.assessment = assessment;
+        this.nota = nota;
+        this.dataEntrega = dataEntrega;
+        this.comentarios = comentarios;
+    }
+
+    public Aluno getAluno() { 
+        return this.aluno; 
+    }
+    public Assessment getAssessment() { 
+        return this.assessment; 
+    }
+    public double getNota() { 
+        return this.nota; 
+    }
+    public String getDataEntrega() { 
+        return this.dataEntrega; 
+    }
+    public String getComentarios() { 
+        return this.comentarios; 
+    }
+}
