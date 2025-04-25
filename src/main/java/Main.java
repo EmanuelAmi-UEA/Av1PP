b4093e2306236f3f245f90eecd10d3945ae0b657
Emanuel Ami da Silva Hernandez <eadshe.snf24@uea.edu.br>
2025-04-25 22:05:08 +0000

    Los

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -101,7 +101,7 @@ public class Main {
       System.out.println("1 - " + curso.getNome());
       System.out.println("2 - " + curso2.getNome());
       System.out.println("0 - Sair");
-      System.out.print("Opção: ");
+      System.out.print("Digite um número de curso: ");
 
       int escolhaCurso = scanner.nextInt();
       scanner.nextLine(); // Consumir a quebra de linha
